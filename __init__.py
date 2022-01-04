4782f7ae390924829888cea1bc9f451f774e625a
Evgeny <evgenykondratev@gmail.com>
2022-01-04 12:09:26 +0300

    Проверка сесии на сервере

--- __init__.py
+++ __init__.py
@@ -1,9 +1,24 @@
-from flask import Flask
+from flask import Flask, session
+from flask_session import Session
+
 app = Flask(__name__)
+
+SESSION_TYPE = 'redis'
+app.config.from_object(__name__)
+Session(app)
 
 @app.route('/')
 def hello_Flask():
     return 'Hello Flask!!!'
+
+@app.route('/set/')
+def set():
+    session['key'] = 'value'
+    return 'ok'
+
+@app.route('/get/')
+def get():
+    return session.get('key', 'not set')
 
 if __name__ == '__main__':
     app.run()
