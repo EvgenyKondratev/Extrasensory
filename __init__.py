1f2cc555df2836498546d116c11d64a888c43a74
Evgeny <evgenykondratev@gmail.com>
2022-01-04 22:48:03 +0300

    Добавлена проверка при отправке данных формы

--- __init__.py
+++ __init__.py
@@ -1,19 +1,28 @@
 from flask import Flask, session, request, redirect, url_for, render_template
 from flask_session import Session
-import redis
+from flask_wtf import FlaskForm
+from wtforms import IntegerField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, NumberRange
+
 import random
 
 app = Flask(__name__)
 
 SESSION_TYPE = 'redis'
 app.config.from_object(__name__)
+app.config['SECRET_KEY'] = 'R2dS*4Wqe#Q2%7mgW{seP7R?'
 Session(app)
 
 extrasensory_count = 30  # Количество экстросенсов
 beg = 10  # Минимальное двухзначное число
 end = 99  # Максимальное двухзначное число
-# d_ext = {}
-# d_ext_level = {}
+
+
+class MyValueForm(FlaskForm):
+    myvalue = IntegerField(label='Введите загаданное двухзначное число',
+                           validators=[DataRequired(), NumberRange(min=beg, max=end)])
+    submit = SubmitField(label='Отправить')
+
 
 def get_extrasensory_data():
     ext_accuracy = {}
@@ -23,6 +32,7 @@ def get_extrasensory_data():
         ext_vals[i] = session.get(f'ext_vals:{i}', [])
     return (ext_accuracy, ext_vals)
 
+
 def extrasensory_predict():
     for i in range(extrasensory_count):
         random_integer = random.randint(beg, end)
@@ -30,16 +40,14 @@ def extrasensory_predict():
 
         if not session.modified:
             session.modified = True
-        # if i not in d_ext:
-        #     d_ext[i] = []
-        # random_integer = random.randint(beg, end)
-        # d_ext[i].append(random_integer)
+
     session['num'] = session.get('num', 0) + 1
+
 
 def get_extrasensory_value(example_count=3):
     ret = {}
     j = 0
-    while(j < example_count):
+    while j < example_count:
         idx = random.randint(0, extrasensory_count - 1)
         if idx not in ret:
             vals = session.get(f'ext_vals:{idx}', [])
@@ -47,6 +55,7 @@ def get_extrasensory_value(example_count=3):
             j += 1
 
     return ret
+
 
 def calc_extrasensory_level(value):
     for i in range(extrasensory_count):
@@ -58,14 +67,11 @@ def calc_extrasensory_level(value):
             if acc > 0:
                 session[f'ext_accuracy:{i}'] = acc - 1
 
-# @app.route('/result', methods=['GET', 'POST'])
-# def input_val():
-#     if request.method == 'POST':
-#     else:
 
 @app.route('/input_val', methods=['GET', 'POST'])
 def input_val():
-    if request.method == 'POST':
+    form = MyValueForm()
+    if form.validate_on_submit():
         vals_count = len(session.get('myvals', []))
         num = session.get('num', 0)
 
@@ -83,22 +89,16 @@ def input_val():
                                myvals=session.get('myvals', []),
                                ext_accuracy=ext_accuracy,
                                ext_vals=ext_vals)
-        #redirect(url_for('index'))
-
-        # s = ''
-        # for key, val in d_level.items():
-        #     s += f'<p>Экстрасенс {key}, уровень достоверности: {val}</p>'
-        # return s
-
-    elif request.method == 'GET':
+    else:
         vals_count = len(session.get('myvals', []))
         num = session.get('num', 0)
         print(vals_count)
         print(num)
-        if(vals_count == num):
+        if vals_count == num:
             extrasensory_predict()
         exp_resp = get_extrasensory_value()
-        return render_template('input_val.html', exp_resp=exp_resp) # ext_accuracy=ext_accuracy, ext_vals=ext_vals)
+        return render_template('input_val.html', form=form, exp_resp=exp_resp)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -106,7 +106,6 @@ def index():
         return redirect(url_for('input_val'))
     elif request.method == 'GET':
         num = session.get('num', 0)
-        # print(num)
         if num == 0:
             session['myvals'] = []
             for i in range(extrasensory_count):
@@ -116,30 +115,12 @@ def index():
         vals_count = len(session.get('myvals', []))
         ext_accuracy, ext_vals = get_extrasensory_data()
 
-        # for i in range(extrasensory_count):
-        #     ext_accuracy[i] = session.get(f'ext_accuracy:{i}', 0)
-        #     ext_vals[i] = session.get(f'ext_vals:{i}', [])
-
         return render_template('index.html',
                                vals_count=vals_count,
                                myvals=session.get('myvals', []),
                                ext_accuracy=ext_accuracy,
                                ext_vals=ext_vals)
 
-@app.route('/set/')
-def set():
-    session['key'] = 'value'
-    return 'ok'
-
-@app.route('/get/')
-def get():
-    # r = redis.Redis(host='localhost', port=6379, db=0)
-    # s = ''
-    # for key in r.scan_iter():
-    #     s += f'{key}: {r.get(key)}<br/>'
-    #     print(key, r.get(key))
-    # return s if s != '' else 'not set' #session.get('key', 'not set')
-    return session.get('key', 'not set')
 
 if __name__ == '__main__':
     app.run()
